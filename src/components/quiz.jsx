41c8b28ae627ab2cad91d1c115b0537d8df5e5cd
minhduyhodev <duyhm.ce190378@gmail.com>
2026-05-17 15:48:25 +0700

    Refactor Quiz component to manage user answers and navigation; enhance button styles

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Quiz() {
   const questionBank = [
     {
@@ -22,25 +24,61 @@ function Quiz() {
     },
   ];
 
-  const [optionSelected, setOptionSelected] = useState("None");
+  const initiaAnswers = [null, null, null];
+
+  const [] = useState();
+
+  const [userAnswers, setUserAnswers] = useState(initiaAnswers);
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const selectedAnswer = userAnswers[currentQuestion];
 
   function handleSelectedOption(option) {
-    setOptionSelected = option;
+    const newUserAnswer = [...userAnswers];
+    newUserAnswer[currentQuestion] = option;
+
+    setUserAnswers(newUserAnswer);
   }
+
+  function goToNext() {
+    setCurrentQuestion(currentQuestion + 1);
+  }
+
+  function goToPrev() {
+    if (currentQuestion > 0) setCurrentQuestion(currentQuestion - 1);
+  }
+
   return (
     <div>
       <h2> Question 1</h2>
-      <p className="question"> {questionBank[0].question}</p>
+      <p className="question"> {questionBank[currentQuestion].question}</p>
 
-      {questionBank[0].options.map((option) => (
-        <button className="option" onClick>
-          {option}
+      {questionBank[currentQuestion].options.map((option) => (
+        <button
+          key={option}
+          className={`option ${selectedAnswer === option ? "selected" : ""}`}
+          onClick={() => handleSelectedOption(option)}
+          type="button"
+        >
+          {" "}
+          {option}{" "}
         </button>
       ))}
 
       <div className="nav-buttons">
-        <button> Previos </button>
-        <button> Next </button>
+        <button
+          onClick={goToPrev}
+          disabled={currentQuestion == 0}
+          type="button"
+        >
+          {" "}
+          Previous{" "}
+        </button>
+        <button onClick={goToNext} disabled={!selectedAnswer} type="button">
+          {" "}
+          Next{" "}
+        </button>
       </div>
     </div>
   );
